272ee57f7944c2914110c5568bc2576380e253fa
BABE-PC\Acer <ilovepeem117@hotmail.com>
2018-08-05 16:36:52 +0700

    Laravel Training Bootcamp

--- app/Http/Controllers/DemoController.php
+++ app/Http/Controllers/DemoController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User as UserMod; 
+use \App\Exports\BladeExport;
 
 class DemoController extends Controller
 {
     public function index()
     {
-        return view('template');
+        return "Demo one";
     }
 
     public function demotwo()
@@ -25,4 +27,25 @@ class DemoController extends Controller
     {
         return "Method GET, POST, PUT/PATCH, DELETE : demofour";
     }
+
+    public function testlinenoti()
+    {
+        $line_noti_token = "FfuuzwLNLQ4kZ6HBD2Dz0eRNaBX5voMasU3Z8foqiS4";
+        
+        $message = array(
+          'message' => "Hello World",//required message
+          'stickerPackageId'=> 2,
+          'stickerId'=> 34
+        );
+        
+        notify_message($message,$line_noti_token);
+        
+        return 'ok';
+    }
+
+    public function testexcel(){
+        $user = UserMod::all();
+        return \Excel::download(new BladeExport($user->toArray()), 'invoices.xlsx');
+ }
+
 }
